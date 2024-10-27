95990aa9663242f40cd58e69376e66bad72d2640
Sammi Turner <ms.sammi.turner@gmail.com>
2024-10-27 21:42:18 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'terminal-background': '#000000',
+        'terminal-text': '#ffffff',
+        'terminal-prompt': '#00ff00',
+      },
+    },
   },
   plugins: [],
 }
